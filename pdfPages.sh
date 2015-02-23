1218dd3c0eb42de0e2291b7c0c31f347c16db005
nkh <navid.khazaee@gmail.com>
2015-02-23 09:05:07 +0330

    First commit - working

---
+++ pdfPages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+totalPages=0
+for i in "$@"
+do
+
+while read a
+do
+    pages=`pdfinfo "$a" 2>/dev/null | grep Pages | cut -d ":" -f 2 | tr -d ' '`
+    echo "$a: $pages"
+    totalPages=$((totalPages+pages))
+done <<< "$(ls "$i" | egrep -i "*.pdf")"
+
+done
+echo "Total pages: $totalPages"
